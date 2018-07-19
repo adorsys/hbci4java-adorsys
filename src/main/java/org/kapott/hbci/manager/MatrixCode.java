e7baa386b477aab9d91872d6878026a219159219
Alexander Geist <age@adorsys.de>
2018-07-19 13:30:35 +0200

    fixed compile error

--- src/main/java/org/kapott/hbci/manager/MatrixCode.java
+++ src/main/java/org/kapott/hbci/manager/MatrixCode.java
@@ -7,33 +7,33 @@
 
 package org.kapott.hbci.manager;
 
-import org.kapott.hbci.comm.Comm;
+
+import static org.kapott.hbci.comm.CommPinTan.ENCODING;
 
 /**
  * Klasse zum Parsen von Matrix-Codes.
  */
-public class MatrixCode
-{
+public class MatrixCode {
     private String mimetype = null;
     private byte[] image = null;
-    
+
     /**
      * ct.
+     *
      * @param data die Rohdaten aus dem HHDuc als String.
      * @throws Exception wenn die Daten nicht als Bild geparst werden konnten.
      */
-    public MatrixCode(String data) throws Exception
-    {
-        this(data != null ? data.getBytes(Comm.ENCODING) : null);
+    public MatrixCode(String data) throws Exception {
+        this(data != null ? data.getBytes(ENCODING) : null);
     }
-    
+
     /**
      * ct.
+     *
      * @param data die Rohdaten aus dem HHDuc als Byte-Array.
      * @throws Exception wenn die Daten nicht als Bild geparst werden konnten.
      */
-    public MatrixCode(byte[] data) throws Exception
-    {
+    public MatrixCode(byte[] data) throws Exception {
         if (data == null || data.length < 100)
             throw new Exception("invalid matrix code");
 
@@ -41,55 +41,56 @@ public class MatrixCode
         // Mime-Type
         {
             byte[] b = new byte[2];
-            System.arraycopy(data,offset,b,0,2);
-            
+            System.arraycopy(data, offset, b, 0, 2);
+
             int len = Integer.parseInt(this.decode(b));
             b = new byte[len];
             offset += 2;
-            
-            System.arraycopy(data,offset,b,0,len);
-            this.mimetype = new String(b,Comm.ENCODING);
+
+            System.arraycopy(data, offset, b, 0, len);
+            this.mimetype = new String(b, ENCODING);
             offset += len;
         }
-        
+
         // Bild-Daten
         {
             // Die Groesse brauchen wir hier nicht ermitteln,
             // weil das Bild ja genau bis zum Ende des Byte-Array geht
-            offset +=2;
-            int len = data.length -  offset;
+            offset += 2;
+            int len = data.length - offset;
             byte[] b = new byte[len];
-            
-            System.arraycopy(data,offset,b,0,len);
+
+            System.arraycopy(data, offset, b, 0, len);
             this.image = b;
         }
     }
-    
+
     /**
      * Decodiert die Bytes als String.
+     *
      * @param bytes die Bytes.
      * @return der String.
      */
-    private String decode(byte[] bytes)
-    {
+    private String decode(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
-        for (int i=0;i<bytes.length;++i)
-        {
-            sb.append(Integer.toString(bytes[i],10));
+        for (int i = 0; i < bytes.length; ++i) {
+            sb.append(Integer.toString(bytes[i], 10));
         }
         return sb.toString();
     }
-    
+
     /**
      * Liefert die Rohdaten des Bildes.
+     *
      * @return image die Rohdaten des Bildes.
      */
     public byte[] getImage() {
         return image;
     }
-    
+
     /**
      * Liefert den Mimetype des Bildes.
+     *
      * @return mimetype
      */
     public String getMimetype() {
