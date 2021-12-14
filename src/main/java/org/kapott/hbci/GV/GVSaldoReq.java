c96e8a29a9d4e4d68002386bb9272c1a5a82e803
Saltuk Kezer <saltukkezer@gmail.com>
2021-12-14 14:49:04 +0100

    Refactors GVSaldoReq

--- src/main/java/org/kapott/hbci/GV/GVSaldoReq.java
+++ src/main/java/org/kapott/hbci/GV/GVSaldoReq.java
@@ -28,13 +28,53 @@ import org.kapott.hbci.structures.Konto;
 import org.kapott.hbci.structures.Saldo;
 import org.kapott.hbci.structures.Value;
 
-import java.text.SimpleDateFormat;
+import java.util.Currency;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class GVSaldoReq extends AbstractHBCIJob {
+    private static final Map<String, Function<String, String>> typeToRawResponseValueMapper;
+
+    static {
+        typeToRawResponseValueMapper = new HashMap<>();
+
+        Function<String, String> mappingForCurrencyAndAmount = (type) -> {
+            switch (type) {
+                case "ready":
+                    return "booked.BTG";
+                case "unready":
+                    return "pending.BTG";
+                default:
+                    return type;
+            }
+        };
+        Function<String, String> mappingForRest = (type) -> {
+            switch (type) {
+                case "ready":
+                    return "booked";
+                case "unready":
+                    return "pending";
+                default:
+                    return type;
+            }
+        };
+
+        typeToRawResponseValueMapper.put("amount", mappingForCurrencyAndAmount);
+        typeToRawResponseValueMapper.put("currency", mappingForCurrencyAndAmount);
+        typeToRawResponseValueMapper.put("creditDebit", mappingForRest);
+        typeToRawResponseValueMapper.put("date", mappingForRest);
+        typeToRawResponseValueMapper.put("time", mappingForRest);
+    }
+
+    private static final BiFunction<String, String, String> AMOUNT_TEMPLATE = (response, type) -> String.format("%s.%s.value", response, typeToRawResponseValueMapper.get("amount").apply(type));
+    private static final BiFunction<String, String, String> CURRENCY_TEMPLATE = (response, type) -> String.format("%s.%s.currency", response, typeToRawResponseValueMapper.get("currency").apply(type));
+    private static final BiFunction<String, String, String> CREDIT_DEBIT_TEMPLATE = (response, type) -> String.format("%s.%s.CreditDebit", response, typeToRawResponseValueMapper.get("creditDebit").apply(type));
+    private static final BiFunction<String, String, String> DATE_TEMPLATE = (response, type) -> String.format("%s.%s.date", response, typeToRawResponseValueMapper.get("date").apply(type));
+    private static final BiFunction<String, String, String> TIME_TEMPLATE = (response, type) -> String.format("%s.%s.time", response, typeToRawResponseValueMapper.get("time").apply(type));
 
     public GVSaldoReq(HBCIPassportInternal passport, String name) {
         super(passport, name, new GVRSaldoReq(passport));
@@ -71,73 +111,93 @@ public class GVSaldoReq extends AbstractHBCIJob {
         info.konto.curr = result.get(header + ".curr");
         passport.fillAccountInfo(info.konto);
 
-        info.ready = new Saldo();
-        String cd = result.get(header + ".booked.CreditDebit");
-        String bookedValue = result.get(header + ".booked.BTG.value") != null ? result.get(header + ".booked.BTG" +
-            ".value") : "0";
-        String st = (cd.equals("D") ? "-" : "") + bookedValue;
-        info.ready.value = new Value(
-            st,
-            result.get(header + ".booked.BTG.curr"));
-        info.ready.timestamp = HBCIUtils.strings2DateTimeISO(result.get(header + ".booked.date"),
-            result.get(header + ".booked.time"));
+        //TODO: Put all relevant fields - amount, currency, credit, date and time into an object to be parsed of the map
+        info.ready = createSaldo(result, AMOUNT_TEMPLATE.apply(header, "ready"),
+            CURRENCY_TEMPLATE.apply(header, "ready"),
+            CREDIT_DEBIT_TEMPLATE.apply(header, "ready"),
+            DATE_TEMPLATE.apply(header, "ready"),
+            TIME_TEMPLATE.apply(header, "ready"));
 
-        cd = result.get(header + ".pending.CreditDebit");
-        if (cd != null) {
-            String pendingValue = result.get(header + ".booked.BTG.value") != null ? result.get(header + ".pending" +
-                ".BTG.value") : "0";
-            st = (cd.equals("D") ? "-" : "") + pendingValue;
-            info.unready = new Saldo();
-            info.unready.value = new Value(
-                st,
-                result.get(header + ".pending.BTG.curr"));
-            info.unready.timestamp = HBCIUtils.strings2DateTimeISO(result.get(header + ".pending.date"),
-                result.get(header + ".pending.time"));
-        }
+        Optional.ofNullable(result.get(header + ".pending.CreditDebit")).ifPresent(creditDebit -> {
+            info.unready = createSaldo(result,
+                AMOUNT_TEMPLATE.apply(header, "unready"),
+                CURRENCY_TEMPLATE.apply(header, "unready"),
+                CREDIT_DEBIT_TEMPLATE.apply(header, "unready"),
+                DATE_TEMPLATE.apply(header, "unready"),
+                TIME_TEMPLATE.apply(header, "unready"));
+        });
 
-        st = result.get(header + ".kredit.value");
-        if (st != null) {
-            info.kredit = new Value(
-                st,
-                result.get(header + ".kredit.curr"));
-        }
+        info.kredit = createValue(result,
+            AMOUNT_TEMPLATE.apply(header, "kredit"),
+            CURRENCY_TEMPLATE.apply(header, "kredit"),
+            false);
+        info.available = createValue(result,
+            AMOUNT_TEMPLATE.apply(header, "available"),
+            CURRENCY_TEMPLATE.apply(header, "available"),
+            false);
+        info.used = createValue(result,
+            AMOUNT_TEMPLATE.apply(header, "used"),
+            AMOUNT_TEMPLATE.apply(header, "used"),
+            false);
 
-        st = result.get(header + ".available.value");
-        if (st != null) {
-            info.available = new Value(
-                st,
-                result.get(header + ".available.curr"));
-        }
+        retrieveReservedBalanceInfoFromUPD().ifPresent(reservedBalanceInfo -> {
+            //TODO: Edge case check if amount is number
+            Optional.ofNullable(reservedBalanceInfo.get("Balance.VOR.Amount")).ifPresent(
+                amount -> {
+                    amount = amount.replace(",", ".");
 
-        st = result.get(header + ".used.value");
-        if (st != null) {
-            info.used = new Value(
-                st,
-                result.get(header + ".used.curr"));
-        }
+                    info.reserved = new Saldo();
+                    info.reserved.value = new Value(amount, reservedBalanceInfo.getOrDefault("Balance.VOR.Currency", "EUR"));
 
-        retrieveReservedBalanceInfoFromUPD().ifPresent( reservedBalanceInfo -> {
-            String reservedAmount = reservedBalanceInfo.get("Balance.VOR.Amount").replace(",",".");
-            if(reservedAmount != null){
-                info.reserved = new Saldo();
-                info.reserved.value = new Value(
-                    reservedAmount,
-                    reservedBalanceInfo.get("Balance.VOR.Currency")
-                );
-                info.reserved.timestamp = HBCIUtils.string2DateISO(reservedBalanceInfo.get("Balance.VOR.Date"), "yyyyMMdd");
-            }
+                    //TODO: Shouldn't be in the past
+                    Optional.ofNullable(reservedBalanceInfo.get("Balance.VOR.Date")).ifPresent(
+                        date -> info.reserved.timestamp = HBCIUtils.string2DateISO(reservedBalanceInfo.get("Balance.VOR.Date"), "yyyyMMdd")
+                    );
+                }
+            );
         });
 
         ((GVRSaldoReq) (jobResult)).store(info);
     }
 
-    private Optional<Map<String,String>> retrieveReservedBalanceInfoFromUPD(){
-        return Optional.ofNullable(passport.getUPD().get("KInfo.accountdata")).map( accountData -> {
+    private boolean negativeSaldo(Map<String, String> result, String creditDebitKey) {
+        return result.get(creditDebitKey).equals("D");
+    }
+
+    private Saldo createSaldo(Map<String, String> result, String amountKey, String currencyKey, String creditDebitKey, String dateKey, String timeKey) {
+        Saldo saldo = new Saldo();
+        saldo.value = createValue(result, amountKey, currencyKey, negativeSaldo(result, creditDebitKey));
+        saldo.timestamp = HBCIUtils.strings2DateTimeISO(result.get(dateKey), result.get(timeKey));
+        return saldo;
+    }
+
+    private Value createValue(Map<String, String> result, String amountKey, String currencyKey, boolean negative) {
+        return Optional.ofNullable(result.get(amountKey)).map(amount -> {
+            String currencyValue = result.get(currencyKey);
+            String possibleNegativeAmount = (negative ? "-" : "") + amount;
+            if (checkCurrency(currencyValue)) {
+                return new Value(possibleNegativeAmount, currencyValue);
+            }
+            return new Value(possibleNegativeAmount);
+        }).orElse(null);
+    }
+
+    private boolean checkCurrency(String currencyCode) {
+        try {
+            Currency.getInstance(currencyCode);
+        } catch (Exception any) {
+            return false;
+        }
+        return true;
+    }
+
+    private Optional<Map<String, String>> retrieveReservedBalanceInfoFromUPD() {
+        return Optional.ofNullable(passport.getUPD().get("KInfo.accountdata")).map(accountData -> {
                 final var reservedBalancePattern = Pattern.compile("(Balance\\.VOR\\..*?)=(.*?(?=;))");
                 final var matcher = reservedBalancePattern.matcher(accountData);
 
-                Map<String,String> reservedBalanceInfo = new HashMap<>();
-                while(matcher.find()){
+                Map<String, String> reservedBalanceInfo = new HashMap<>();
+                while (matcher.find()) {
                     final var key = matcher.group(1);
                     final var value = matcher.group(2);
                     reservedBalanceInfo.put(key, value);
